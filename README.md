339ff4c97356dd074f033127d1686f4b795d1fcc
greentis <lklcs2004@gmail.com>
2024-09-26 11:09:36 +0800

    hexagonal map

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # comp4451-game
+
+# install npm, three.js, npx
+# "npx vite" in terminal to run
--- main.js
+++ main.js
@@ -26,28 +26,33 @@ scene.add(light);
 
 
 //
-// Render map
+// Render map (map.js)
 //
 const map = new THREE.Object3D();
 scene.add(map);
 
+var radius = 8;
+var spacing = 1.8;
 
-const geometry = new THREE.CylinderGeometry(
-    5,5,2,6
-);
-for (var j = -3; j <= 3; j++){
-    for (var i = -3; i <= 3; i++){
+// tile.js
+for (var q = -radius; q <= radius; q++){
+    for (var r = -radius; r <= 4; r++){
+        // Keep the radius = 6
+        var s = 0 - q - r;
+        if (Math.abs(s) > radius) continue;  
+
+        // Tile contruction
+        const geometry = new THREE.CylinderGeometry(5,5,2,6);
         const material = new THREE.MeshPhongMaterial({emissive:0x000000});
         const tile = new THREE.Mesh(geometry,material);
         tile.scale.set(0.2, 0.2, 0.2);
-        tile.position.x = i * 0.9;
-        tile.position.z = j * 3.6;
-        if (i % 2 == 0) tile.position.z += 1.8;
-        
+        tile.position.x = q * spacing + r * spacing * Math.cos(Math.PI /3);
+        tile.position.z = r * spacing * Math.cos(Math.PI /6);
+
+        // Add tile to map
         map.add(tile);
     }
 }
-
 
 
 
